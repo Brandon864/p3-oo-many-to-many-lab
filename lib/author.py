c6cfbe3fc8ab2244a0094ee9b6c9937728162373
Brandon864 <ojbrandon2345@mail.com>
2025-05-25 00:52:25 +0300

    Fix Book class initialization and implement Author and Contract classes

---
+++ lib/author.py
@@ -0,0 +1,32 @@
+# lib/author.py
+from contract import Contract
+from book import Book
+
+class Author:
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("name must be a string")
+        self._name = value
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
---
+++ lib/book.py
@@ -0,0 +1,17 @@
+# lib/book.py
+class Book:
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise Exception("title must be a string")
+        self._title = value
